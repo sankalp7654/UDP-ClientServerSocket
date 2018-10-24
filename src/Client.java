841b22652f31e3b26a8ba61a9d069468b9022ca6
Sankalp Saxena <sankalp.saxena.sta@gmail.com>
2018-10-25 02:00:57 +0530

    initial commit

---
+++ src/Client.java
@@ -0,0 +1,34 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) throws Exception {
+
+		DatagramSocket datagramSocket = new DatagramSocket();
+		
+		System.out.println("Enter the number to be sent to Server");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		System.out.println("Data sent to Server: " + number);
+		
+		byte byteMessage [] = (number+"").toString().trim().getBytes();		
+		InetAddress address = InetAddress.getLocalHost();
+		int port = 9999;
+		
+		DatagramPacket sendPacket = new DatagramPacket(byteMessage, byteMessage.length, address, port);
+		datagramSocket.send(sendPacket);
+		
+		byte [] receivedByte = new byte[1024];
+		DatagramPacket response = new DatagramPacket(receivedByte, receivedByte.length);
+		datagramSocket.receive(response);
+		
+		String receivedData = new String(receivedByte, 0, receivedByte.length);
+		System.out.println("Below response is received from Server\nThe square is: " + receivedData);
+		
+	}
+
+}
---
+++ src/Server.java
@@ -0,0 +1,33 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Server {
+
+	public static void main(String[] args) throws Exception{
+		
+		DatagramSocket serverSocket = new DatagramSocket(9999);
+		
+		System.out.println("Server is up and running");
+		
+		byte [] receivedByte = new byte[1024];
+		DatagramPacket receivedPacket = new DatagramPacket(receivedByte, receivedByte.length);
+		serverSocket.receive(receivedPacket);
+		
+		String receivedData = new String(receivedByte, 0, receivedByte.length);
+		System.out.println("Packet received from Client: " + receivedData);
+		
+		int number = Integer.parseInt(receivedData.trim());
+		
+		byte[] square = ((number*number) + "").toString().trim().getBytes();
+		
+		InetAddress serverAddress = InetAddress.getLocalHost();
+		DatagramPacket sendPacket = new DatagramPacket(square, square.length, serverAddress, receivedPacket.getPort());
+		serverSocket.send(sendPacket);
+		
+		System.out.println("Square has been sent to Client");
+		
+
+	}
+
+}
